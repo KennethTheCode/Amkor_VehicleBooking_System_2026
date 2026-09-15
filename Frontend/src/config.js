7a32870f6e57b6b6c810093d15e01c596735ea99
KennethTheCode <117273823+KennethTheCode@users.noreply.github.com>
2026-09-15 09:24:15 +0800

    sa

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,6 +1,6 @@
 let API_BASE;
 
-if (window.location.hostname === "172.16.1.13") {
+if (window.location.hostname === "172.16.1.15") {
     API_BASE = `${window.location.origin}/backend`;
 } else {
     API_BASE = "http://localhost/Amkor_VehicleBooking_System_2026/Backend";
